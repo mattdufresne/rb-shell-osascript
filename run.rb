fc7f726060c66c80d327c29b6acfef87b8250860
Matt Dufresne <matt@pvep.com>
2021-03-08 21:47:31 -0500

    find isbn

--- run.rb
+++ run.rb
@@ -1,8 +1,8 @@
-def run_applescript(file,script)
+def run_applescript(file,script,args)
   command = <<~SCRIPT
     tell application "Adobe InDesign 2021" 
       open "#{file}" 
-      do script "#{script}" with arguments {} language javascript
+      do script "#{script}" with arguments {"#{args}"} language javascript
     end tell
   SCRIPT
   $stderr.puts command
@@ -12,6 +12,13 @@ end
 script = File.expand_path("./script.jsx")
 
 Dir.glob('**/*.indd').each do |f|
+
+  if f.match(/cover/i)
+    args = "cover"
+  elsif f.match(/insides/i)
+    args = "insides"
+  end
+
   file = File.expand_path(f)
-  run_applescript(file,script)
+  run_applescript(file,script,args)
 end
--- script.jsx
+++ script.jsx
@@ -1,1 +1,24 @@
-alert("hi");
+
+if (arguments[0] == 'cover') {
+    getIsbn()
+} else if (arguments[0] == 'insides') {
+    alert(arguments[0] + " == insides")
+}
+
+function getIsbn() {
+    var isbn = null;
+    for (var i = 0; i < app.activeDocument.stories.length; i++){
+        var story = app.activeDocument.stories.item(i);
+        var lines = story.lines;
+
+        for (var j = 0; j < lines.length; j++){
+            var contents = lines[j].contents;
+            if (contents.match(/isbn/i) && !contents.match(/set/i)) {
+                alert(contents)
+                break;
+            }
+        }
+    }
+    return isbn;
+}
+app.activeDocument.close(SaveOptions.no);
